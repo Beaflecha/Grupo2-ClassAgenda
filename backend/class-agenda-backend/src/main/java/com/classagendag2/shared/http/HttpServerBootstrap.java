9d69b9a4c1a4bc57798bc5aa212ce2b3ed62ccce
JoseManuelRuiz <josruisoj@alu.edu.gva.es>
2026-02-23 17:40:53 +0100

    creación de la estructura de carpetas y Punto de entrada

---
+++ backend/class-agenda-backend/src/main/java/com/classagendag2/shared/http/HttpServerBootstrap.java
@@ -0,0 +1,8 @@
+package com.classagendag2.shared.http;
+
+public class HttpServerBootstrap {
+    public void start() throws Exception {
+        // Mensaje de prueba temporal
+        System.out.println("Servidor pendiente de implementación...");
+    }
+}
