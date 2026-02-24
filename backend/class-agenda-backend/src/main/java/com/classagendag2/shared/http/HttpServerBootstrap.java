070dfd2955c0cdc39bc61d6f9634ef1abcdff542
Beaflecha <bea.flecha@hotmail.com>
2026-02-24 03:15:22 +0100

    Update: connect request handler to the server in HttpServerBootstrap

--- backend/class-agenda-backend/src/main/java/com/classagendag2/shared/http/HttpServerBootstrap.java
+++ backend/class-agenda-backend/src/main/java/com/classagendag2/shared/http/HttpServerBootstrap.java
@@ -1,8 +1,27 @@
 package com.classagendag2.shared.http;
 
-public class HttpServerBootstrap {
+import com.classagendag2.shared.config.ServerConfig;
+import com.classagendag2.shared.http.handlers.HealthHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+public final class HttpServerBootstrap {
     public void start() throws Exception {
-        // Mensaje de prueba temporal
-        System.out.println("Servidor pendiente de implementación...");
+        // 1.Preguntamos porqué puerto debemos abrir el servidor
+        int serverPort = ServerConfig.port();
+
+        // 2.Creamos la dirección física (IP de la máquina + Puerto
+        InetSocketAddress serverAddress = new InetSocketAddress(serverPort);
+        HttpServer httpServer = HttpServer.create(serverAddress, 0);
+
+        // 3.REGISTRAMOS LA RUTA "Si alguien pide /health, pásale la llamada al HealthHandler"
+        httpServer.createContext("/health", new HealthHandler());
+
+        // 4.Encendemos el servidor para que empiece a escuchar infinitamente
+        httpServer.start();
+
+        System.out.println("ClassAgenda API running on http://localhost:" + serverPort);
+
     }
 }
